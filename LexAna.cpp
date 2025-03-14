fe48b86a419271a3bdb937e7d190722697197376
Cindy Garcia <Cinderg166@gmail.com>
2025-03-14 15:02:25 -0700

    Eliminated whitespace elimination, and added block comment elimination

--- LexAna.cpp
+++ LexAna.cpp
@@ -4,11 +4,18 @@
 
 using namespace std;
 
+string analyze() {
+    string separators[] = {
+        ";", ",", ".", ":", "::", "{", "}", "[", "]", "(", ")", "<>", 
+    };
+}
+
 int main(void){
     //Variables
     string filename;
     string content;
     char c;
+    char prev;
     bool iscomment = false;
 
     //Getting filename from user
@@ -27,20 +34,20 @@ int main(void){
 
     //Reading file content into string
     while(inFile.get(c)) {
-        if(c != ' ') {//Eliminating whitespace
-            if(c == '/' && content.at(content.length()-1) == '/') { //Checking if a comment has started
-                iscomment = true;
-                content.pop_back();
-            }
-
-            if(iscomment == true && c == '\n') { //Checking if a comment has ended
-                iscomment = false;
-            }
-
-            if(iscomment == false && c != '\n') { //Eliminating newlines
-                content.push_back(c);
-            }
+        if(c == '/' && content.at(content.length()-1) == '/' || c == '*' && content.at(content.length()-1) == '/') { //Checking if a comment has started
+            iscomment = true;
+            content.pop_back();
         }
+
+        if(iscomment == true && c == '\n' || iscomment == true && c == '/' && prev = '*' ) { //Checking if a comment has ended
+            iscomment = false;
+        }
+
+        if(iscomment == false) {
+                content.push_back(c);
+        }
+        prev = c;
+        
     }
 
     //Printing out file content for debugging purposes
