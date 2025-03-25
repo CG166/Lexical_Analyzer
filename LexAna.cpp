ac3f5da781407ba4fb0a97b8a96208a319186e0f
Cindy Garcia <Cinderg166@gmail.com>
2025-03-24 19:32:24 -0700

    added all comments

--- LexAna.cpp
+++ LexAna.cpp
@@ -7,93 +7,101 @@
 
 using namespace std;
 
+//Functions
+//This Function checks whether a string is an integer by looping through the string and check each character
 bool isInt(string lexeme) {
-    bool isint = false;
-
-    for(int i = 0; i < lexeme.length(); i++){
-        if(isdigit(lexeme[i])) {
-            isint = true;
+    bool isint = false; //isInt is defaulted to false
+    for(int i = 0; i < lexeme.length(); i++){ //Loops through each char of the lexeme
+        if(isdigit(lexeme[i])) { //Checks if a char is an integer
+            isint = true; //isInt is set/remains true if the char is an Int
         } else {
-            return false;
+            return false; //The entire function immediatly returns false if any char is not an Int
         }
     }
-    return isint;
+    return isint; //Returns a true isInt if every char of the lexeme is an integer
 }
 
+//This function checks if a string/lexeme is a separator, operator, keyword, or identifier and prints out the corresponding message
 string analyze(string lexeme) {
-    string answer = " =";
+    string answer = " ="; //Partial answer format (used for printing purposes only)
 
-    string separators[] = {
+    string separators[] = { //Array of strings containing all C++ separators
         ";", ",", ".", ":", "::", "{", "}", "[", "]", "(", ")", "->", "#"
     };
-    string operators[] = {
+    string operators[] = { //Array of strings containing all C++ operators
         "+", "-", "*", "/", "%", "==", "!=", "<", ">", "<=", ">=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>",
         "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", "++", "--", "?", ":",
         "&", "*", "sizeof", ",", "static_cast", "dynamic_cast", "const_cast", "reinterpret_cast"
     };
-    std::string keywords[] = {
+    std::string keywords[] = { //Array of strings containing all C++ Keywords 
         "if", "else", "switch", "case", "default", "while", "do", "for", "break", "continue", "return", "goto",
         "auto", "register", "static", "extern", "mutable", "thread_local","int", "char", "float", "double", "void",
         "bool", "wchar_t", "long", "short", "signed", "unsigned", "long long", "typedef", "using", "struct", "union", "enum", "class", "template",
         "try", "catch", "throw", "namespace", "new", "delete", "inline", "friend", "virtual", "override", "final", "explicit", "const", "constexpr", "noexcept"
     };
 
-    int Slength = sizeof(separators) / sizeof(separators[0]);
-    int Olength = sizeof(operators) / sizeof(operators[0]);
-    int Klength = sizeof(keywords) / sizeof(keywords[0]);
+    int Slength = sizeof(separators) / sizeof(separators[0]); //Computes and strores the length of the separator array
+    int Olength = sizeof(operators) / sizeof(operators[0]); //Computes and strores the length of the separator array
+    int Klength = sizeof(keywords) / sizeof(keywords[0]); //Computes and strores the length of the separator array
 
-    if(isInt(lexeme)){
-        return '"' + lexeme + '"' + answer + " integer\n\n";
+    //This checks if the lexeme is an integer
+    if(isInt(lexeme)){ //Checks if lexeme is an integer
+        return '"' + lexeme + '"' + answer + " integer\n\n"; //Returns message if lexeme is an integer
     }
 
-    for(int i = 0; i < Slength; i++) {
-        if(lexeme == separators[i]) {
-            return '"' + lexeme + '"' + answer + " separator\n\n";
+    //This checks if the lexeme is a separator
+    for(int i = 0; i < Slength; i++) { //Iterates through the separator array
+        if(lexeme == separators[i]) { //Compares the lexeme to every separator in the separator array
+            return '"' + lexeme + '"' + answer + " separator\n\n"; //Returns message if lexeme matches a separator
         }
     }
 
-    for(int i = 0; i < Olength; i++) {
-        if(lexeme == operators[i]) {
-            return '"' + lexeme + '"' + answer + " operator\n\n";
+    //This checks if the lexeme is an operator
+    for(int i = 0; i < Olength; i++) { //Iterates through the operator array
+        if(lexeme == operators[i]) { //Compares the lexeme to every operator in the operator array
+            return '"' + lexeme + '"' + answer + " operator\n\n"; //Returns message if lexeme matches an operator
         }
     }
 
-    for(int i = 0; i < Klength; i++) {
-        if(lexeme == keywords[i]) {
-            return '"' + lexeme + '"' + answer + " keyword\n\n";
+    //This checks if the lexeme is a keyword
+    for(int i = 0; i < Klength; i++) { //Iterates through the keyword array
+        if(lexeme == keywords[i]) { //Compares the lexeme to every keyword in the keyword array
+            return '"' + lexeme + '"' + answer + " keyword\n\n"; //Returns message if lexeme matches a keyword
         }
     }
 
-
+    //This sets and returns the message that the lexeme is an identifier if that is the only remaining option
     answer = '"' + lexeme + '"' + answer +  " identifier" + "\n\n";
 
     return answer;
 }
 
-bool isSeparator(char c) {
-    string separators[] = {
+//This is a boolean function that returns true or false if an inputed char is a separator.
+bool isSeparator(char c) { //Funtion takes in a char as a parameter
+    string separators[] = { //Array of strings containing all C++ separators
         ";", ",", ".", ":", "::", "{", "}", "[", "]", "(", ")", "->", "#"
     };
 
-    int length = sizeof(separators) / sizeof(separators[0]);
+    int length = sizeof(separators) / sizeof(separators[0]); //Computes and strores the length of the separator array
 
-    for(int i = 0; i < length; i++) {
-        if(string(1, c) == separators[i]) {
-            return true;
+    for(int i = 0; i < length; i++) { //Iterates through the separator array
+        if(string(1, c) == separators[i]) { //Checks if the char matches any separator
+            return true; //Returns true if the char matches any separator
         }
     }
-    return false;
+    return false; //Returns false if the char matches any separator
 }
 
-vector<string> split(string contents) {
+//The function spits a sentence string into a vector of strings that contains each word of the sentence string (no whitespaces)
+vector<string> split(string contents) { //Function takes in a string(sentence as a parameter)
     istringstream stream(contents);
     vector<string> indivs;
     string lexeme;
 
-    while (stream >> lexeme) {
-        indivs.push_back(lexeme);
+    while (stream >> lexeme) { //Reads every lexeme(string) from the content sentence(string) (no newlines or whitespaces)
+        indivs.push_back(lexeme); //pushes lexeme into vector
     }
-    return indivs;
+    return indivs; //returns vector of lexemes
 }
 
 int main(void){
@@ -107,8 +115,8 @@ int main(void){
 
 
     //Getting filename from user
-    cout << "Filename: ";
-    getline(cin, filename);
+    cout << "Filename: "; //Prompts user to input filename
+    getline(cin, filename); //Reads user input into filename variable
     cout << "\n";
 
     //Opening file
@@ -116,39 +124,40 @@ int main(void){
     inFile.open(filename);
 
     //Error message
-    if(inFile.fail()){
-        cout << "The file has failed to open.\n";
+    if(inFile.fail()){//Checks if file has failed to open
+        cout << "The file has failed to open.\n"; //Prints out error message
         return 1;
     }
 
     //Reading file content into string
-    while(inFile.get(c)) {
+    while(inFile.get(c)) { //Reading in file content one char at a time
         if(c == '/' && content.at(content.length()-1) == '/' || c == '*' && content.at(content.length()-1) == '/') { //Checking if a comment has started
-            iscomment = true;
-            content.pop_back();
+            iscomment = true; //Sets the isComment variable to true
+            content.pop_back(); //Removing comment backslash from content string
         }
 
+        //Check if the given char is not part of a comment and is a sepparator
         if(iscomment == false && isSeparator(c) == true) {
-            content.push_back(' ');
-            content.push_back(c);
-            content.push_back(' ');
-        } else if (iscomment == false && c != '\n') {
-            content.push_back(c);
+            content.push_back(' '); //If the char is a separator a whitespace is pushed into the content string before and after the
+            content.push_back(c); //char in order to properly seperate the lexemes within the content string for future separation purposes.
+            content.push_back(' '); 
+        } else if (iscomment == false && c != '\n') {//Checks if the given char is not part of a comment and not a newline
+            content.push_back(c); //Pushes char into content string
         }
 
         if(iscomment == true && c == '\n' || iscomment == true && c == '/' && prev == '*' ) { //Checking if a comment has ended
-            iscomment = false;
+            iscomment = false; //Sets the isComment variable to true
         }
-        prev = c;
+        prev = c; //Stores the char before moving on for future comparison purposes
         
     }
 
     //Breaking up content string into a vector of individual lexemes
     lexemes = split(content);
 
-    //Analyzing lexemes
+    //Iterates through the vector of lexemes, tokenizing each lexeme and outputing the corresponding message
     for (const auto& str : lexemes) {
-        cout << analyze(str);
+        cout << analyze(str); //Prints out correct messages in "<lexemes> = <tokens>" format
     }
 
     //Closing file
